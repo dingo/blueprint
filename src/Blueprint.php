cc44a3e2a20e122a3392e37d5dde161ce85aa35a
Michael Bladowski <info@macropage.de>
2016-12-28 00:54:28 +0100

    add hostname to output
    fix https://github.com/dingo/blueprint/issues/17

--- src/Blueprint.php
+++ src/Blueprint.php
@@ -131,6 +131,8 @@ class Blueprint
         $contents = '';
 
         $contents .= $this->getFormat();
+        $contents .= $this->line(1);
+        $contents .= 'HOST: ' . \Config::get('api.protocol') . '://' . \Config::get('api.domain') . '/' . \Config::get('api.prefix');
         $contents .= $this->line(2);
         $contents .= sprintf('# %s', $name);
         $contents .= $this->line(2);
